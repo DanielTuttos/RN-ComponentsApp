a90d3f893814d24380a0a07800c154ae65699aa8
Daniel Romero <tutorialesdanielromero@gmail.com>
2024-03-26 21:39:05 -0500

    :construction: adding directories, navigation and icons

---
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+import { Airplane } from '../../icons/icons';
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Airplane />
+    </View>
+  );
+};
